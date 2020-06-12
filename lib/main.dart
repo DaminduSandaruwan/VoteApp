9ee84c589bc10598c1e900c9a59edc1afe7b8a5d
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-06-12 21:13:12 +0530

    Navigation to home screen

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,7 @@ class VoteApp extends StatelessWidget {
                 color: Colors.white,
                 onPressed: (){
                   print('Result Screen');
+                  Navigator.pushReplacementNamed(context, '/home');
                 },
               ),
             ),
